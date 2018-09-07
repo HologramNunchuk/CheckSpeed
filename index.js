dab51b4f4c3534f140e4a29715d907109275f6bb
Jaime <jaimesof82@gmail.com>
2018-09-06 19:49:08 -0700

    CheckSpeed function, but with more concise code

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ checkSpeed();
 
 function checkSpeed(speed) {
   const speedLimit = 70;
-  const errorPoints = 5;
+  const errors = 5;
 
-  if (speed < speedLimit + errorPoints) console.log("Driving Safe!!");
-  else {
-    const points = Math.floor((speed - speedLimit) / errorPoints);
-
-    if (points >= 12) console.log("Slow Down!!");
-    else console.log("Points", points);
+  if (speed < speedLimit + errors) {
+    console.log("Driving great!!");
+    return;
   }
+  const points = Math.floor((speed - speedLimit) / errors);
+  if (points >= 12) console.log("You gotta slow down!!");
+  else console.log("Points", points);
 }
